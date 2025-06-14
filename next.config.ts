58fb53405af4eb62bee91cf7e97418893f87e8a6
Ross <528266131@qq.com>
2025-06-14 11:34:48 -0400

    dev: change config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     unoptimized: true,
+    domains: ["jimmieluo.com"],
     remotePatterns: [
       {
         protocol: "https",
